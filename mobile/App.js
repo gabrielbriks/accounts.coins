f9ba04626a1e89b3025c2a4d7f29e74c3d5c65ea
gabrielbriks <greis9010@gmail.com>
2020-02-16 19:14:18 -0300

    Construindo e estilizando Home principal, adicionando cards

--- mobile/App.js
+++ mobile/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Routes from "./src/routes";
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Account$Map</Text>
-    </View>
+   <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#4169E1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 35,
-    fontWeight: "bold",
-    color: '#FFF',
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#4169E1',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     fontSize: 35,
+//     fontWeight: "bold",
+//     color: '#FFF',
+//   }
+// });
---
+++ mobile/src/services/api.js
@@ -0,0 +1,12 @@
+// import React from "react";
+// import { StyleSheet, Image, View, Animated } from "react-native";
+
+// export default function Main(){
+
+//   return (
+
+//     <View />
+
+//   );
+
+// };
