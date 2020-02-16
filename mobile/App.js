aeea21d5a850ff7c3b64954823b73f9ae0b23ea3
gabrielbriks <greis9010@gmail.com>
2020-02-15 23:21:59 -0300

    test, Color default do app

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Account$Map</Text>
     </View>
   );
 }
@@ -12,8 +12,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4169E1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: '#FFF',
+  }
 });
