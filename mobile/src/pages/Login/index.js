f5ea28a4d2cccc88c3cbf31b79d0a31731791b7c
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-02-19 13:39:32 -0300

    Login page - Implementando comunicação com a api com rota 'signin' e ajustando styles

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -5,46 +5,60 @@ import styled from "styled-components";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { NavigationSwitchScreenProps } from 'react-navigation';
 
+import api from "../../services/api";
+
 /* A propiedade navigation, vem de uma forma automatica para todas as pages da nossa aplication */
 export default function Login({ navigation }){
 
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     
-    async function singInPress(){
+    async function signInPress(){
         console.log(login);
         console.log(password);
+
+        const response = await api.post('/signin', {
+            params:{
+                login,
+                password
+            }
+        });
+
         navigation.navigate('Main');
     }
 
     return (
         <Container>
-            <ContainerSingIn>
+            <ContainerSignIn>
                 <TextInput style ={styles.input}
                     placeholder="Login"
-                    placeholderTextColor="#000"
+                    placeholderTextColor={'rgba(0, 0, 0, 0.25)'}
                     autoCapitalize="none"
                     autoCorrect={false}
                     value = {login}
                     onChangeText = {setLogin}
+                    autoFocus={true}
+                    required={true}
                 />
                 <TextInput style={styles.input}
-                    placeholder="Senha"                    
-                    placeholderTextColor="#000"
+                    placeholder="Senha" 
+                    placeholderTextColor={'rgba(0, 0, 0, 0.25)'}                   
                     autoCapitalize="none"
                     autoCorrect={false}
                     secureTextEntry
                     onChangeText={setPassword}
                     value ={password}
+                    maxLength={12}
+                    
                 />
                 <TouchableOpacity 
                     title="Entrar"
                     style={styles.button}
-                    onPress={ singInPress}
+                    onPress={ signInPress}
                 >
                     <ButtonText>Entrar</ButtonText>
                 </TouchableOpacity>
-            </ContainerSingIn>
+            </ContainerSignIn>
         </Container>
     );
 
@@ -55,9 +69,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         width: 300,
         height: 40,
-        marginTop: 20    ,
-        borderRadius: 25,
-        textAlign: "center"
+        marginTop: 20,
+        borderRadius: 25,        
+       fontWeight: "bold",
+        textAlign: "center",
+        alignItems: "center"
     },
     button:{
         backgroundColor: '#333',
@@ -78,7 +94,7 @@ const Container = styled.View`
     text-align: center;
 `;
 
-const ContainerSingIn = styled.View`
+const ContainerSignIn = styled.View`
     flex:1;
     margin-top: 270px;
     align-self: center;
