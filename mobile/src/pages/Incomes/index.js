a6b10306a94d7be4b52fe39f739e9cab8d169163
gabrielbriks <greis9010@gmail.com>
2020-02-23 19:52:34 -0300

    Incomes Page - Constução da page Incomes

--- mobile/src/pages/Incomes/index.js
+++ mobile/src/pages/Incomes/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign'
+import styled from "styled-components";
 
-import { Container } from './styles';
+import { Container, CardContainer, ContainerTitle, CardTitle, CardValue } from './styles';
 
 export default function Incomes() {
   
@@ -10,8 +12,45 @@ export default function Incomes() {
   }
 
   return (
-    <Container>
-      <Text style={{fontSize: 32}}>Page Incomes</Text>
-    </Container>
+    <ScrollView style={styles.container}>
+      <Container>
+        <Text style={{fontSize: 32}}>Page Incomes</Text>
+
+        <CardContainer>{/* This is Card , Has tamanho and background and bordes*/}
+        <ContainerTitle>{/* This is Container title of card, has a line bottom */}
+          <Icon />{/* Icon with action delete */}
+          <CardTitle> Investimentos </CardTitle>{/* Title of Card */}
+        </ContainerTitle>
+        <CardValue>R$ 700,56</CardValue>{/* This is Card Value */}   
+        </CardContainer>
+
+        <CardContainer>{/* This is Card , Has tamanho and background and bordes*/}
+        <ContainerTitle>{/* This is Container title of card, has a line bottom */}
+          <Icon />{/* Icon with action delete */}
+          <CardTitle> Freelancers </CardTitle>{/* Title of Card */}
+        </ContainerTitle>
+        <CardValue>R$ 700,56</CardValue>{/* This is Card Value */}   
+        </CardContainer>
+        
+        <CardContainer>{/* This is Card , Has tamanho and background and bordes*/}
+        <ContainerTitle>{/* This is Container title of card, has a line bottom */}
+          <Icon />{/* Icon with action delete */}
+          <CardTitle> Name Income </CardTitle>{/* Title of Card */}
+        </ContainerTitle>
+        <CardValue>R$ 700,56</CardValue>{/* This is Card Value */}   
+        </CardContainer>
+
+      </Container>
+      
+      <Text style={{fontSize: 72, color: '#fff'}}>
+          Exemple ScrollView ... 
+      </Text>
+    </ScrollView>
+
   );
 }
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: "#4169E1",
+  }
+});
