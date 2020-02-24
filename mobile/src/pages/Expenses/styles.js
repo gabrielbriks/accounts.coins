0b67fda6ba34be16c15fb81b5a76bef5bc60c4ad
gabrielbriks <greis9010@gmail.com>
2020-02-24 15:51:21 -0300

    Expenses listagem - Implementando busca/listagem de Expenses, utilizando backend

--- mobile/src/pages/Expenses/styles.js
+++ mobile/src/pages/Expenses/styles.js
@@ -1,3 +1,4 @@
+import { StyleSheet } from "react-native";
 import styled from 'styled-components';
 
 export const Container = styled.View`
@@ -5,4 +6,44 @@ export const Container = styled.View`
   align-content: center;
   align-items: center;
   margin-top: 38px;
+`;
+
+export const ListItem = styled.View`
+  height: 85px;
+  width: 100%;
+  margin-top: 8px;
+  /* background-color: #0ca2; */
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  border-bottom-color: #333;
+`;
+
+export const ItemDateExpense = styled.Text`
+  position: absolute;
+  margin-left: 275px;
+  margin-top: 18px;
+  opacity: 0.6;
+`;
+
+export const ItemCategory = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  margin-left: 10px;
+  color: #000;
+  opacity: 0.6;  
+`;
+
+export const DescriptionItem = styled.Text`
+  font-size: 18px;  
+  font-weight: 300;
+  margin-left: 30px;
+  color: #000;
+  opacity: 0.6;
+`;
+
+export const ItemValue = styled.Text`
+  font-size: 18px;  
+  font-weight: 300;
+  margin-left: 30px;
+  color: #000;
+  opacity: 0.6;
 `;
